8e4dac492d0d9379672b6c8d85e80b98c7413c55
ValeryKharkov <kharkov.valery@gmail.com>
2023-02-12 22:29:18 +0300

    algorithm_training_1: A,I - выполнено

--- algorithm_training_1/I.py
+++ algorithm_training_1/I.py
@@ -12,18 +12,29 @@ def remove_max(list_arg):
     return list_arg
 
 
-def perimeter(list_arg):
-    sum_ = 0
-    for num in list_arg:
-        sum_ += num
-    return sum_
+# def perimeter(list_arg):
+#     sum_ = 0
+#     for num in list_arg:
+#         sum_ += num
+#     return sum_
 
 
-hole_size = perimeter(de_list)
-brick_size = perimeter(remove_max((abc_list)))
+# hole_size = perimeter(de_list)
+# brick_size = perimeter(remove_max((abc_list)))
+#
+#
+# if max(abc_list) > max(de_list) or hole_size < brick_size:
+#     print('NO')
+# else:
+#     print('YES')
 
+ab_list = remove_max(abc_list)
+a = ab_list[0]
+b = ab_list[1]
+d = de_list[0]
+e = de_list[1]
 
-if max(abc_list) > max(de_list) or hole_size < brick_size:
-    print('NO')
-else:
+if a < d and b < e or a < e and b < d:
     print('YES')
+else:
+    print('NO')
