3182b671579908a7767a8b7b64cd3d52fb981d49
ValeryKharkov <kharkov.valery@gmail.com>
2023-04-07 17:02:15 +0300

    Создание БД при помощи SQLite

---
+++ devman/lesson_1.py
@@ -0,0 +1,55 @@
+print('\u001b[36m')
+print("""
+```
+                _-o#&&*''''?d:>b\\_
+              _o/'`''  '',, dMF9MMMMMHo_
+           .o&#'        `'MbHMMMMMMMMMMMHo.
+         .o'' '         vodM*$&&HMMMMMMMMMM?.
+        ,'              $M&ood,~'`(&##MMMMMMH\\
+       /               ,MMMMMMM#b?#bobMMMMHMMML
+      &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk
+     ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|`*L
+    |               |MMMMMMMMMMMMMMMMMMMMbMH'   T,
+    $H#:            `*MMMMMMMMMMMMMMMMMMMMb#}'  `?
+    ]MMH#             ''*''''*#MMMMMMMMMMMMM'    -
+    MMMMMb_                   |MMMMMMMMMMMP'     :
+    HMMMMMMMHo                 `MMMMMMMMMT       .
+    ?MMMMMMMMP                  9MMMMMMMM}       -
+    -?MMMMMMM                  |MMMMMMMMM?,d-    '
+     :|MMMMMM-                 `MMMMMMMT .M|.   :
+      .9MMM[                    &MMMMM*' `'    .
+       :9MMk                    `MMM#'        -
+         &M}                     `          .-
+          `&.                             .
+            `~,   .                     ./
+                . _                  .-PP
+                  '`--._,dd###pp='''
+ ```
+ """)
+print("""
+```
+                  .ovr:HMM#?:`' >b\\_
+              .,:&Hi' `'   '' \\\\|&bSMHo_
+            oHMMM#*}          `?&dMMMMMMHo.
+         .dMMMH'''''           ,oHH*&&9MMMM?.
+        ,MMM*'                 `*M\\bd<|'*&#MH\\
+       dHH?'                   :MMMMMM#bd#odMML
+      H' |\\                  `dMMMMMMMMMMMMMM9Mk
+     JL/'7+,.                `MMMMMMMMMMMMMMMH9ML
+    -`Hp     '               |MMMMMMMMMMMMMMMMHH|:
+    :  \\\\#M#d?                `HMMMMMMMMMMMMMMMMH.
+    .   JMMMMM##,              ``*''''*#MMMMMMMMH
+    -. ,MMMMMMMM6o_                    |MMMMMMMM':
+    :  |MMMMMMMMMMMMMb\\                 TMMMMMMT :
+    .   ?MMMMMMMMMMMMM'                 :MMMMMM|.`
+    -    ?HMMMMMMMMMM:                  HMMMMMM\\|:
+     :     9MMMMMMMMH'                 `MMMMMP.P.
+      .    `MMMMMMT''                   HMMM*''-
+       -    TMMMMM'                     MM*'  -
+        '.   HMM#                            -
+          -. `9M:                          .'
+            -. `b,,    .                . '
+              '-\\   .,               .-`
+                  '-:b~\\\\_,oddq==--'
+```
+""")
